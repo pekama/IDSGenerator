46c8f84a5f526e5b14109cb464346d701c759653
eyal fisher <eyalf2005@gmail.com>
2015-01-16 14:32:24 +0000

    copied idsgenerator from pekama

--- utilities/generateids/generateids.py
+++ utilities/generateids/generateids.py
@@ -1,10 +1,8 @@
-__author__ = 'eyal'
-import os
-
+import subprocess
 
 class IdsFIller:
 
-    def fill_ids(self, jar_path ,original_ids_path, data_xml_path, output_path):
-        os.system('java -jar "%s" "%s" "%s" "%s"' % (jar_path, original_ids_path, data_xml_path, output_path,))
+    def fill_ids(self, jar_path, original_ids_path, data_xml_path, output_path):
+        subprocess.call(['java', '-jar', jar_path, original_ids_path, data_xml_path, output_path])
 
 
